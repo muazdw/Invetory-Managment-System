366d89e61c72098f1a35726577be7e015cb8220c
muazdw <muazabdullau1@gmail.com>
2026-07-03 23:44:36 +0200

    feat: add @neondatabase/serverless package with initial configuration and dependencies
    - Created package.json for @neondatabase/serverless with version 1.1.0, including scripts, dependencies, and metadata.
    - Added package-lock.json to lock dependencies for the project.
    - Updated root package.json to include @neondatabase/serverless as a dependency.

--- ims-api/src/app.module.ts
+++ ims-api/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { ProductsModule } from './products/products.module';
 import { InventoryModule } from './inventory/inventory.module';
 import { SuppliersModule } from './suppliers/suppliers.module';
@@ -13,24 +14,36 @@ import { CommonModule } from './common/common.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+
     CommonModule,
+
     TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get('DATABASE_HOST'),
-        port: parseInt(config.get('DATABASE_PORT', '5432'), 10),
-        username: config.get('DATABASE_USER'),
-        password: config.get('DATABASE_PASS'),
-        database: config.get('DATABASE_NAME'),
-        autoLoadEntities: true,
-        synchronize: true, 
-        
-         ssl: {
-      rejectUnauthorized: false,
-    },
-      }),
+      useFactory: (config: ConfigService) => {
+        const isProd = config.get('NODE_ENV') === 'production';
+
+        return {
+          type: 'postgres',
+          host: config.get('DATABASE_HOST'),
+          port: parseInt(config.get('DATABASE_PORT', '5432'), 10),
+          username: config.get('DATABASE_USER'),
+          password: config.get('DATABASE_PASS'),
+          database: config.get('DATABASE_NAME'),
+
+          autoLoadEntities: true,
+
+          // 🔥 IMPORTANT: kurrë true në prod
+          synchronize: false,
+
+          // 🔥 FIX SSL ERROR
+          ssl: isProd
+            ? { rejectUnauthorized: false }
+            : false,
+        };
+      },
     }),
+
     ProductsModule,
     InventoryModule,
     SuppliersModule,
